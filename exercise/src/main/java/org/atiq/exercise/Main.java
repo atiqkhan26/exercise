274d568ce123169238159ae6ef69ca8d4356b54e
atiq <atiqurrahman.khan@gmail.com>
2017-12-30 23:44:39 +0000

    Code submit

---
+++ exercise/src/main/java/org/atiq/exercise/Main.java
@@ -0,0 +1,34 @@
+package org.atiq.exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+	public static boolean testCounts(Map<String, Integer> wordMap, List<Integer> counts) {
+		boolean result = false;
+		if (wordMap.size() == counts.size()) {
+			int index = 0;
+			for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
+				System.out.printf("%s - %d\n", entry.getKey(), entry.getValue());
+				if (entry.getValue() != counts.get(index++)) {
+					return result;
+				}
+			}
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	public static void main(String[] args) {
+		String str = "This is a statement, and so is this.";
+		List<Integer> values = Arrays.asList(2, 2, 1, 1, 1, 1);
+		
+		WordCounter counter = new WordCounter();
+		Map<String, Integer> counts = counter.countRepetitions1(str);
+		System.out.printf("Test %s", Main.testCounts(counts, values) ? "passed" : "failed");
+	}
+	
+}
---
+++ exercise/src/main/java/org/atiq/exercise/WordCounter.java
@@ -0,0 +1,31 @@
+package org.atiq.exercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WordCounter {
+	
+	static private Pattern pattern = Pattern.compile("(\\b[^\\s]+\\b)");
+	
+	public Map<String, Integer> countRepetitions1(String str) {
+		Map<String, Integer> result = null;
+		if (str != null && !str.isEmpty()) {
+			Matcher matcher = pattern.matcher(str);
+			result = new LinkedHashMap<>();
+			while (matcher.find()) {
+				String word = matcher.group().toLowerCase();
+				Integer value = result.get(word);
+				if (value == null) {
+					result.put(word, 1);
+				} else {
+					result.put(word, value + 1);
+				}
+			}
+		}
+		
+		return result;
+	}
+
+}
